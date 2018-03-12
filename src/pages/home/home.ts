74dab0bd553e0c0e711e45b634a2025dd6827dca
Dileep Kottakota <Dileep.Kottakota@synechron.com>
2018-03-12 19:04:05 +0530

    ionic first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { EmployeeInfo } from '../information/employee-info';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,33 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  items: any = [];
 
+  constructor(public navCtrl: NavController) {
+    this.initializeItems();
+  }
+
+  itemSelected(item: any) {
+    this.navCtrl.push(EmployeeInfo);
+  }
+
+  initializeItems() {
+    this.items = ["Emp1", "Emp2", "Emp3", "Emp4", "Emp5", "Emp6", "Emp7", "Emp8", "Emp9"];
+  }
+
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
   }
 
 }
---
+++ src/pages/information/employee-info.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+    selector: 'employee-info',
+    templateUrl: 'employee-info.html'
+})
+export class EmployeeInfo {
+    constructor(public navCtrl: NavController) {
+    }
+
+}
